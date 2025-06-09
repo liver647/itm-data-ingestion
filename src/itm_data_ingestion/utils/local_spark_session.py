df897f449404480083b8ce59a61149a7b7523c0e
Aliou Sow <alioujigga@hotmail.com>
2025-06-09 18:38:52 +0200

    Update pyproject.toml & local spark session

--- src/itm_data_ingestion/utils/local_spark_session.py
+++ src/itm_data_ingestion/utils/local_spark_session.py
@@ -1,13 +1,17 @@
 """A module containing the local spark session utils."""
 
-import logging
+import logging, re
 
 import delta
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
+from itm_data_ingestion.infrastructure.settings import Settings
 
-def get_spark_session() -> SparkSession:
+
+def get_spark_session(
+        config: Settings
+) -> SparkSession:
     """
     Get the spark session.
 
@@ -21,42 +25,43 @@ def get_spark_session() -> SparkSession:
     py4jlogger = logging.getLogger("py4j")
     py4jlogger.setLevel(logging.WARNING)
 
-    app_name = "New Client Data Ingestion Job Application"
+    app_name = config.spark_app_name
+    account_name = re.search(r"AccountName=([^;]+)", config.azure_storage_connection_string).group(1)
+    account_key  = re.search(r"AccountKey=([^;]+)",  config.azure_storage_connection_string).group(1)
 
-    conf = SparkConf().setAll(
-        [
-            # General Configuration
-            ("spark.sql.sources.partitionColumnTypeInference.enabled", "false"),
-            ("spark.sql.session.timeZone", "UTC"),
-            ("spark.sql.parquet.compression.codec", "snappy"),
-            ("spark.sql.sources.partitionOverwriteMode", "dynamic"),
-            ("spark.sql.legacy.timeParserPolicy", "CORRECTED"),
-            ("spark.sql.optimizer.dynamicPartitionPruning.enabled", "true"),
-            ("spark.sql.join.preferSortMergeJoin", "true"),
-            ("spark.serializer", "org.apache.spark.serializer.KryoSerializer"),
-            # Enable Adaptive Query Execution
-            ("spark.sql.adaptive.enabled", "true"),
-            ("spark.sql.adaptive.skewJoin.enabled", "true"),
-            ("spark.sql.adaptive.coalescePartitions.enabled", "true"),
-            # Enable Delta Lake
-            ("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension"),
-            ("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog"),
-            # Apply Auto Broadcast Join if the smallest dataframe is less than 25 MB
-            ("spark.sql.autoBroadcastJoinThreshold", "26214400"),
-            ("spark.sql.adaptive.autoBroadcastJoinThreshold", "26214400"),
-            # Writing Optimization
-            ("spark.databricks.delta.optimizeWrite.enabled", "true"),
-            ("spark.databricks.delta.optimizeWrite.binSize", "32"),  # 32MB
-            ("spark.databricks.delta.autoCompact.enabled", "true"),
-            ("spark.databricks.delta.autoCompact.maxFileSize", "268435456"),  # 256 MB
-            # Hadoop Configuration
-            ("spark.hadoop.parquet.block.size", str(1024 * 1024 * 16)),  # 16MB
-        ]
+    conf = (
+        SparkConf()
+        # General Configuration
+        .set("spark.sql.session.timeZone", "UTC")
+        .set("spark.sql.sources.partitionColumnTypeInference.enabled", "false")
+        .set("spark.sql.parquet.compression.codec", "snappy")
+        .set("spark.sql.sources.partitionOverwriteMode", "dynamic")
+        .set("spark.sql.legacy.timeParserPolicy", "CORRECTED")
+        .set("spark.sql.optimizer.dynamicPartitionPruning.enabled", "true")
+        .set("spark.sql.join.preferSortMergeJoin", "true")
+        .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        # Enable Adaptive Query Execution
+        .set("spark.sql.adaptive.enabled", "true")
+        .set("spark.sql.adaptive.skewJoin.enabled", "true")
+        .set("spark.sql.adaptive.coalescePartitions.enabled", "true")
+        # Enable Delta Lake
+        .set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .set("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    
     )
-
-    builder = SparkSession.builder.config(conf=conf).appName(app_name).enableHiveSupport()
+    builder = (
+        SparkSession.builder
+        .appName(app_name)
+        .config(conf=conf)
+        .enableHiveSupport()
+    )
+    
     spark = delta.configure_spark_with_delta_pip(builder).getOrCreate()
-
+    spark.conf.set(
+        f"fs.azure.account.key.{account_name}.blob.core.windows.net",
+        account_key,
+    )
+    
     spark.sparkContext.setLogLevel("INFO")
 
     return spark
